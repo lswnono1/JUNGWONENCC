7549a137f8917c2b00a3fecbee765f3550e9896f
lswnono1 <lswnono1@gmail.com>
2026-07-15 22:49:22 +0900

    Install closing SQLite connections before recovery

--- launch_v10_9_link_mode.py
+++ launch_v10_9_link_mode.py
@@ -1,5 +1,7 @@
+from v10_9_link_mode.connection_patch import install_connection_patch
 from v10_9_link_mode.migration_patch import install_patch
 
+install_connection_patch()
 install_patch()
 
 from v10_9_link_mode.main import main
