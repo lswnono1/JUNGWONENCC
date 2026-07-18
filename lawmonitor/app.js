61f3a999bedc5b2d415535a3a48698860c9c35eb
lswnono1 <lswnono1@gmail.com>
2026-07-18 16:50:26 +0900

    모바일 앱 v1.3 기능 로더 연결

--- lawmonitor/app.js
+++ lawmonitor/app.js
@@ -1,6 +1,6 @@
 'use strict';
 (async () => {
-  const parts = ["app-core-1.txt", "app-core-2.txt", "app-core-3.txt", "app-core-4.txt"];
+  const parts = ["app-core-1.txt", "app-core-2.txt", "app-core-3.txt", "app-core-4.txt", "app-core-5.txt"];
   try {
     const responses = await Promise.all(parts.map((name) => fetch(`./${name}`, {cache:'no-store'})));
     for (const response of responses) if (!response.ok) throw new Error(`앱 구성 파일 오류 HTTP ${response.status}`);
